b04825747ad39f8b93e6c1307dd23106065b82be
Damjan Pavlica <mudroljub@gmail.com>
2017-01-27 13:57:30 +0100

    samo cistim

--- src/io/mish.js
+++ src/io/mish.js
@@ -1,38 +1,14 @@
 const mish = {
-  _stisnut: false,
+  stisnut: false,
 
   init: () => {
     document.onmousemove = e => {
-      mish.x = e.pageX;
-      mish.y = e.pageY;
+      mish.x = e.pageX
+      mish.y = e.pageY
     }
-    document.onmousedown = () => mish.stisnut = true;
-    document.onmouseup = () => mish.stisnut = false;
-  }(),
-
-  get stisnut() {
-    return mish._stisnut;
-  },
-
-  set stisnut(bool) {
-    mish._stisnut = bool;
-  },
-
-  get x() {
-    return mish._x;
-  },
-
-  set x(x) {
-    mish._x = x;
-  },
-
-  get y() {
-    return mish._y;
-  },
-
-  set y(y) {
-    mish._y = y;
-  }
+    document.onmousedown = () => mish.stisnut = true
+    document.onmouseup = () => mish.stisnut = false
+  }()
 }
 
 export default mish
