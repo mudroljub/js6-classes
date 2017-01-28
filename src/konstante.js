55efafd1e07226b36836ee314bc6594abdb8b79f
Damjan Pavlica <mudroljub@gmail.com>
2017-01-28 15:05:26 +0100

    sredjen root

--- src/konstante.js
+++ src/konstante.js
@@ -1,4 +1,5 @@
-export const root = '/';
+export const root = location.pathname
+
 export const KRUZNICA = Math.PI * 2;
 export const MOBILNI = /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent);
 
--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+import {UI} from './core/UI'
+
 import BombasScena from './scene/BombasScena'
 import NemciIzRovova from './scene/NemciIzRovova'
 import FranjoKluzScena from './scene/FranjoKluzScena'
@@ -24,5 +26,45 @@ import SavoNoc from './scene/SavoNoc'
 import RanjenikScena from './scene/RanjenikScena'
 import RanjenikPaljba from './scene/RanjenikPaljba'
 
-const trenutnaScena = new BombasScena()
-trenutnaScena.start()
+const scene = {
+  "Bombaš": BombasScena,
+  "Nemci iz rovova": NemciIzRovova,
+  "Avijacija 1942": FranjoKluzScena,
+  "Bekstvo iz Jasenovca": JasenovacScena,
+  "Partizanska mornarica": PomorskaScena,
+  "Ubij okupatora": OtpisaniScena,
+  "Avijacija 1944": Scena1944,
+  "Tenk ide": TenkicIde,
+  "Tenkići": TenkiciScena,
+  "Savo mitraljezac": SavoScena,
+  "Ranjenik na Sutjesci": RanjenikScena
+}
+
+const sablon = () => {
+  let meni = ``
+  Object.keys(scene).map(key => {
+      meni += `<button value='${key}' class='full'>${key}</button><br>`
+  })
+  return `
+    <h1>Glavni meni</h1>
+    ${meni}
+  `
+}
+
+new UI(sablon, 'ui').render()
+
+const on = (e, fn) => document.addEventListener(e, fn)
+
+on('click', e => new scene[e.target.value]().start())
+
+// function pustiScenu(scena) {
+//   let izabranaScena = scene[scena] || meni
+//   window.location.hash = scene[scena] ? scena : ''
+//   trenutnaScena.stop()
+//   trenutnaScena = izabranaScena
+//   trenutnaScena.start()
+//   window.scroll(0, 0)
+// }
+
+// const trenutnaScena = new scene.BombasScena()
+// trenutnaScena.start()
